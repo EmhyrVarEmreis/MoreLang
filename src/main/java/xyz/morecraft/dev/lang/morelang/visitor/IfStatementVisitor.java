937c5cf418261ad82493066b201c384241116011
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2017-06-01 13:42:06 +0200

    Added support for multiple ifStatements

--- src/main/java/xyz/morecraft/dev/lang/morelang/visitor/IfStatementVisitor.java
+++ src/main/java/xyz/morecraft/dev/lang/morelang/visitor/IfStatementVisitor.java
@@ -4,7 +4,9 @@ import xyz.morecraft.dev.lang.morelang.MoreLangGrammarParser;
 import xyz.morecraft.dev.lang.morelang.object.statement.IfStatement;
 import xyz.morecraft.dev.lang.morelang.visitor.proto.MoreLangGrammarBaseVisitorCustom;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class IfStatementVisitor extends MoreLangGrammarBaseVisitorCustom<IfStatement> {
 
@@ -13,10 +15,11 @@ public class IfStatementVisitor extends MoreLangGrammarBaseVisitorCustom<IfState
         ExpressionVisitor expressionVisitor = new ExpressionVisitor();
         StatementVisitor statementVisitor = new StatementVisitor();
         final List<MoreLangGrammarParser.BodyBlockContext> bodyBlockContexts = ctx.bodyBlock();
+        final MoreLangGrammarParser.IfStatementContext obj = ctx.ifStatement();
         return new IfStatement(
                 ctx.expression().accept(expressionVisitor),
                 convert(bodyBlockContexts.get(0).body().statement(), statementVisitor),
-                bodyBlockContexts.size() > 1 ? convert(bodyBlockContexts.get(1).body().statement(), statementVisitor) : null
+                bodyBlockContexts.size() > 1 ? convert(bodyBlockContexts.get(1).body().statement(), statementVisitor) : (Objects.isNull(obj) ? null : Collections.singletonList(obj.accept(this)))
         );
     }
 
